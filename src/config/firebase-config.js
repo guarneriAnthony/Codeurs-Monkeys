950136c6c37a13b1446b2fed72ef9cf91bbd9fc7
Siga <guarneriAnthony@outlook.fr>
2024-05-19 16:43:06 +0200

    Prepare the component Typo with the style text and colors

---
+++ src/config/firebase-config.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDV19cwld3vI5eEF6WaikYpDE3rigBZ4cs",
+  authDomain: "codeurs-monkeys-25bfd.firebaseapp.com",
+  projectId: "codeurs-monkeys-25bfd",
+  storageBucket: "codeurs-monkeys-25bfd.appspot.com",
+  messagingSenderId: "432279287507",
+  appId: "1:432279287507:web:e8f17e851c225303858626"
+};
+
+const app = initializeApp(firebaseConfig);
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,18 @@
 import { Seo } from "@/ui/components/seo";
+import { Typography } from "@/ui/design-system/typography/typography";
 
 export default function Home() {
   return (
     <>
-    
-      <Seo  title="Codeurs Monkeys" description="Codeurs Monkeys 'Description'"/>
-
-      <h1>Codeurs Monkeys Hello</h1>
+      <Seo  title="Codeurs Monkeys is now online" description="Codeurs Monkeys 'Description'"/>
+      <div className="space-y-5">
+        <Typography theme="primary" variant="display" component="div">Codeurs Monkeys</Typography>
+        <Typography theme="gray" variant="h1" component="div">Codeurs Monkeys</Typography>
+        <Typography theme="secondary" variant="lead" component="div">Codeurs Monkeys</Typography>
+        <Typography variant="caption4" component="div">Codeurs Monkeys</Typography>
+        <Typography variant="caption4" weight="medium"  component="div">Codeurs Monkeys</Typography>
+      </div>
     </>
+    
   );
 }
